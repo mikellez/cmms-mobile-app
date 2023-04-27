6c20ca98711b5ec9a416d8e718bcce49f34df5b6
zwe <zwezeya02@gmail.com>
2023-04-27 14:11:26 +0800

    add module layout for standardisation

---
+++ components/ModuleLayout/ModuleScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import App from "../../pages/App";
+import { HStack, VStack } from "native-base";
+
+interface ModuleScreenProps extends React.PropsWithChildren {
+    navigation: string
+};
+
+const ModuleScreen = (props: ModuleScreenProps) => {
+    return (
+        <App navigation={props.navigation}>
+            <HStack flex={1} >
+                <VStack flex={1}>{props.children}</VStack>
+            </HStack>
+        </App>
+    );
+};
+
+export { ModuleScreen }
