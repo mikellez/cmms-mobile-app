8add74ebd6299b52df3201e3b517b9efabdcc1d8
Raghava <chinnu4148@gmail.com>
2023-05-09 11:25:56 +0800

    c

--- App.js
+++ App.js
@@ -20,35 +20,42 @@ import Maintenance from "./pages/Maintenance";
 import CompleteRequest from "./pages/Request/Complete";
 import Assets from "./pages/Assets/index";
 import Details from "./pages/Assets/Details";
+import CreateChecklist from "./pages/Checklist/Create";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer ref={navigationRef} linking={linking}>
-            {/*<View style={styles.container}>
+  return (
+    <NavigationContainer ref={navigationRef} linking={linking}>
+      {/*<View style={styles.container}>
         <Login/>
       </View>*/}
-            <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Report" component={Report} />
-                <Stack.Screen name="ViewRequest" component={ViewRequest} />
-                <Stack.Screen name="CreateRequest" component={CreateRequest} />
-                <Stack.Screen name="Maintenance" component={Maintenance} />
-                <Stack.Screen name="Calendar" component={Calendar} />
-                <Stack.Screen name="CreateChecklist" component={CreateChecklist} />
-                <Stack.Screen name="AssignRequest" component={AssignRequest} />
-                <Stack.Screen name="CompleteRequest" component={CompleteRequest} />
-                <Stack.Screen name="ManageRequest" component={ManageRequest} />
-                <Stack.Screen name="QRScan" component={QRScan} />
-                <Stack.Screen name="GuestCreateRequest" component={GuestCreateRequest} />
-                <Stack.Screen name="OfflineRequest" component={OfflineRequest} />
-                <Stack.Screen name="ViewAsset" component={Assets} />
+      <Stack.Navigator
+        initialRouteName="LoginScreen"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Report" component={Report} />
+        <Stack.Screen name="ViewRequest" component={ViewRequest} />
+        <Stack.Screen name="CreateRequest" component={CreateRequest} />
+        <Stack.Screen name="Maintenance" component={Maintenance} />
+        <Stack.Screen name="Calendar" component={Calendar} />
+        <Stack.Screen name="CreateChecklist" component={CreateChecklist} />
+        <Stack.Screen name="AssignRequest" component={AssignRequest} />
+        <Stack.Screen name="CompleteRequest" component={CompleteRequest} />
+        <Stack.Screen name="ManageRequest" component={ManageRequest} />
+        <Stack.Screen name="QRScan" component={QRScan} />
+        <Stack.Screen
+          name="GuestCreateRequest"
+          component={GuestCreateRequest}
+        />
+        <Stack.Screen name="OfflineRequest" component={OfflineRequest} />
+        <Stack.Screen name="ViewAsset" component={Assets} />
         <Stack.Screen name="AssetDetails" component={Details} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
