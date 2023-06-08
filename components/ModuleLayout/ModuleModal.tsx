4e129d99954a63b57a1ed239f05a08522229a570
wenjj2000 <wenjj2000@gmail.com>
2023-06-08 13:40:33 +0800

    added logout confirmation

--- components/ModuleLayout/ModuleModal.tsx
+++ components/ModuleLayout/ModuleModal.tsx
@@ -1,182 +1,194 @@
 import React from "react";
 import { Modal, StyleSheet, Pressable } from "react-native";
-import { Text, IconButton, HStack, View, Modal as NBModal, Icon, VStack } from "native-base";
+import {
+  Text,
+  IconButton,
+  HStack,
+  View,
+  Modal as NBModal,
+  Icon,
+  VStack,
+} from "native-base";
 import AntDesign from "react-native-vector-icons/AntDesign";
-import Feather from "react-native-vector-icons/Feather"
+import Feather from "react-native-vector-icons/Feather";
 
 interface ModuleModalProps extends React.PropsWithChildren {
-    title?: string;
-    isOpen: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  title?: string;
+  isOpen: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 enum ModalIcons {
-    Warning = "warning",
-    Success = "checkcircleo",
-    Offline = "wifi-off",
+  Warning = "warning",
+  Success = "checkcircleo",
+  Offline = "wifi-off",
+  Exit = "logout",
 }
 
 interface ModuleSimpleModalProps extends ModuleModalProps {
-    text: string,
-    feather?: boolean,
-    icon?: ModalIcons
-    onCloseCallback?: Function
-};
+  text: string;
+  feather?: boolean;
+  icon?: ModalIcons;
+  onCloseCallback?: Function;
+}
 
 const ModuleSimpleModal = (props: ModuleSimpleModalProps) => {
+  const closeModal = () => {
+    props.setOpen(false);
+    if (props.onCloseCallback) props.onCloseCallback();
+  };
 
-    const closeModal = () => {
-        props.setOpen(false);
-        if (props.onCloseCallback) props.onCloseCallback();
-    };
+  return (
+    <NBModal isOpen={props.isOpen} onClose={closeModal}>
+      <NBModal.Content>
+        <NBModal.CloseButton />
+        <NBModal.Body padding="4">
+          <VStack space={2}>
+            {props.icon && (
+              <Icon
+                as={props.feather ? Feather : AntDesign}
+                name={props.icon}
+                size="4xl"
+              />
+            )}
 
-    return (
-        <NBModal
-            isOpen={props.isOpen}
-            onClose={closeModal}
-        >
-            <NBModal.Content>
-                <NBModal.CloseButton />
-                <NBModal.Body padding="4">
-                    <VStack space={2}>
-                        {props.icon && <Icon as={props.feather ? Feather :AntDesign} name={props.icon} size="4xl" />}
-
-                        {props.title && (
-                            <Text fontSize="18" fontWeight={600}>
-                                {props.title}
-                            </Text>
-                        )}
-                        {props.text && <Text>{props.text}</Text>}
-                    </VStack>
-                    <View>{props.children}</View>
-                </NBModal.Body>
-            </NBModal.Content>
-        </NBModal>
-    );
+            {props.title && (
+              <Text fontSize="18" fontWeight={600}>
+                {props.title}
+              </Text>
+            )}
+            {props.text && <Text>{props.text}</Text>}
+          </VStack>
+          <View>{props.children}</View>
+        </NBModal.Body>
+      </NBModal.Content>
+    </NBModal>
+  );
 };
 
 const ModuleButtonModal = (props: ModuleSimpleModalProps) => {
+  const closeModal = () => {
+    props.setOpen(false);
+    if (props.onCloseCallback) props.onCloseCallback();
+  };
 
-    const closeModal = () => {
-        props.setOpen(false);
-        if (props.onCloseCallback) props.onCloseCallback();
-    };
+  return (
+    <NBModal isOpen={props.isOpen} onClose={closeModal}>
+      <NBModal.Content>
+        <NBModal.CloseButton />
+        <NBModal.Body padding="4">
+          <VStack space={2}>
+            {props.icon && <Icon as={AntDesign} name={props.icon} size="4xl" />}
 
-    return (
-        <NBModal
-            isOpen={props.isOpen}
-            onClose={closeModal}
-        >
-            <NBModal.Content>
-                <NBModal.CloseButton />
-                <NBModal.Body padding="4">
-                    <VStack space={2}>
-                        {props.icon && <Icon as={AntDesign} name={props.icon} size="4xl" />}
-
-                        {props.title && (
-                            <Text fontSize="18" fontWeight={600}>
-                                {props.title}
-                            </Text>
-                        )}
-                        {props.text && <Text>{props.text}</Text>}
-                    </VStack>
-                    <View>{props.children}</View>
-                </NBModal.Body>
-            </NBModal.Content>
-        </NBModal>
-    );
+            {props.title && (
+              <Text fontSize="18" fontWeight={600}>
+                {props.title}
+              </Text>
+            )}
+            {props.text && <Text>{props.text}</Text>}
+          </VStack>
+          <View>{props.children}</View>
+        </NBModal.Body>
+      </NBModal.Content>
+    </NBModal>
+  );
 };
 
 const ModuleFullPageModal = (props: ModuleModalProps) => {
-    const closeModal = () => {
-        props.setOpen(false);
-    };
+  const closeModal = () => {
+    props.setOpen(false);
+  };
 
-    return (
-        <Modal
-            visible={props.isOpen}
-            onRequestClose={closeModal}
-            transparent={true}
-            animationType="slide"
-        >
-            <Pressable
-                style={{
-                    ...styles.overlay,
-                    display: props.isOpen ? "flex" : "none",
-                }}
-                onPress={closeModal}
-            ></Pressable>
-            <View
-                style={{
-                    ...styles.modalView,
-                }}
-            >
-                <HStack alignItems="center">
-                    <Text style={styles.title}>{props.title}</Text>
-                    <IconButton
-                        _icon={{
-                            as: AntDesign,
-                            name: "close",
-                        }}
-                        _pressed={{
-                            onPress: closeModal,
-                        }}
-                        variant="ghost"
-                        size="lg"
-                        style={styles.button}
-                        colorScheme="black"
-                    />
-                </HStack>
+  return (
+    <Modal
+      visible={props.isOpen}
+      onRequestClose={closeModal}
+      transparent={true}
+      animationType="slide"
+    >
+      <Pressable
+        style={{
+          ...styles.overlay,
+          display: props.isOpen ? "flex" : "none",
+        }}
+        onPress={closeModal}
+      ></Pressable>
+      <View
+        style={{
+          ...styles.modalView,
+        }}
+      >
+        <HStack alignItems="center">
+          <Text style={styles.title}>{props.title}</Text>
+          <IconButton
+            _icon={{
+              as: AntDesign,
+              name: "close",
+            }}
+            _pressed={{
+              onPress: closeModal,
+            }}
+            variant="ghost"
+            size="lg"
+            style={styles.button}
+            colorScheme="black"
+          />
+        </HStack>
 
-                <View>{props.children}</View>
-            </View>
-        </Modal>
-    );
+        <View>{props.children}</View>
+      </View>
+    </Modal>
+  );
 };
 
 const styles = StyleSheet.create({
-    modalView: {
-        position: "absolute",
-        bottom: 0,
-        width: "100%",
-        height: "70%",
-        backgroundColor: "white",
-        borderTopRightRadius: 20,
-        borderTopLeftRadius: 20,
-        borderWidth: 1,
-        borderColor: "#dee2e6",
-        padding: 35,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        flex: 1,
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-        zIndex: 10000,
+  modalView: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: "70%",
+    backgroundColor: "white",
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    borderWidth: 1,
+    borderColor: "#dee2e6",
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    flex: 1,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    zIndex: 10000,
+  },
 
-    button: {
-        marginLeft: "auto",
-    },
+  button: {
+    marginLeft: "auto",
+  },
 
-    title: {
-        fontSize: 18,
-        fontWeight: "600",
-    },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
 
-    overlay: {
-        flex: 1,
-        position: "absolute",
-        height: "100%",
-        width: "100%",
-        // bottom: 0,
-        opacity: 0.25,
-        backgroundColor: "black",
-        zIndex: 100,
-    },
+  overlay: {
+    flex: 1,
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    // bottom: 0,
+    opacity: 0.25,
+    backgroundColor: "black",
+    zIndex: 100,
+  },
 });
 
-export { ModuleSimpleModal, ModuleButtonModal, ModuleFullPageModal, ModalIcons };
+export {
+  ModuleSimpleModal,
+  ModuleButtonModal,
+  ModuleFullPageModal,
+  ModalIcons,
+};
